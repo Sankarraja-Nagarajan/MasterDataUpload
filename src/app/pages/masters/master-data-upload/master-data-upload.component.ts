8ce6d69687df686d45750b4f2542d1946e359e26
vasundhraa <vasundhraakumar@gmail.com>
2024-03-05 17:51:58 +0530

    Updated Dashboard and Login Pages

--- src/app/pages/masters/master-data-upload/master-data-upload.component.ts
+++ src/app/pages/masters/master-data-upload/master-data-upload.component.ts
@@ -1,89 +1,99 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChange, SimpleChanges } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { MasterTableInfoService } from '../../../Services/master-table-info.service';
 import { ActivatedRoute } from '@angular/router';
+import { NbMenuService } from '@nebular/theme';
 
 export interface tableType {
-  position : number,
-  name : string,
-  weight : number,
-  symbol : string
+  position: number,
+  name: string,
+  weight: number,
+  symbol: string
 }
 @Component({
   selector: 'ngx-master-data-upload',
   templateUrl: './master-data-upload.component.html',
   styleUrls: ['./master-data-upload.component.scss']
 })
-export class MasterDataUploadComponent implements OnInit{
+export class MasterDataUploadComponent implements OnInit {
 
-  tableNames = ['Bsak','Ekko','asdf','heih','Ekko','User Credentials','User','e4rw','asdf','cvsa','wrsf','adfa','nhhc','cksr','cswn','cner']
-  // tableNames =['Bsak','Ekko']
-  dataSource : any = [
+  tableNames = ['Ekko', 'Bsak', 'heih', 'User Credentials', 'User', 'e4rw', 'asdf', 'cvsa', 'wrsf', 'adfa', 'nhhc', 'cksr', 'cswn', 'cner']
+  selectedTable: string = this.tableNames[0];
+  workFlowTable: string = 'TCI';
+  dataSource: any = [
     {
-      ID:2,
-      PO_Number : 12412312312,
-      Price : 2424,
-      Currency : 'INR'
+      ID: 2,
+      PO_Number: 12412312312,
+      Price: 2424,
+      Currency: 'INR'
     },
     {
-      ID:2,
-      PO_Number : 12412312312,
-      Price : 2424,
-      Currency : 'INR'
+      ID: 2,
+      PO_Number: 12412312312,
+      Price: 2424,
+      Currency: 'INR'
     },
     {
-      ID:2,
-      PO_Number : 12412312312,
-      Price : 2424,
-      Currency : 'INR'
+      ID: 2,
+      PO_Number: 12412312312,
+      Price: 2424,
+      Currency: 'INR'
     },
     {
-      ID:2,
-      PO_Number : 12412312312,
-      Price : 2424,
-      Currency : 'INR'
+      ID: 2,
+      PO_Number: 12412312312,
+      Price: 2424,
+      Currency: 'INR'
     },
     {
-      ID:2,
-      PO_Number : 12412312312,
-      Price : 2424,
-      Currency : 'INR'
+      ID: 2,
+      PO_Number: 12412312312,
+      Price: 2424,
+      Currency: 'INR'
     },
     {
-      ID:2,
-      PO_Number : 12412312312,
-      Price : 2424,
-      Currency : 'INR'
+      ID: 2,
+      PO_Number: 12412312312,
+      Price: 2424,
+      Currency: 'INR'
     },
 
     {
-      ID:2,
-      PO_Number : 12412312312,
-      Price : 2424,
-      Currency : 'INR'
+      ID: 2,
+      PO_Number: 12412312312,
+      Price: 2424,
+      Currency: 'INR'
     },
-    
+
   ];
-  title : string;
-  displayedColumns : string[];
-  constructor(private _masterTableInfoService : MasterTableInfoService,
-    private _activatedRoute : ActivatedRoute){
-    this.title = 'TCI';
+  title: string;
+  displayedColumns: string[];
+  constructor(private _masterTableInfoService: MasterTableInfoService,
+    private _nbMenu: NbMenuService,
+    private _activatedRoute: ActivatedRoute) {
+    this.title = _masterTableInfoService.tableName;
     this.displayedColumns = _masterTableInfoService.tableInfo[this.title]['displayedColumns']
-  }
 
-  ngOnInit(){
-    this._activatedRoute.queryParams.subscribe({
-      next:(data)=>{
-        if(data.name){
-           this.title = data.name;
-           this.displayedColumns = this._masterTableInfoService.tableInfo[data.name]['displayedColumns'];
-           console.log(this.displayedColumns)
+    _nbMenu.onItemClick().subscribe(x => {
+      if (x != null) {
+        if (x.item.title != 'TCI') {
+          this.workFlowTable = x.item.title;
+          _masterTableInfoService.tableName = this.workFlowTable;
+          this.selectedTable = this.tableNames[0]
+          this.displayedColumns = _masterTableInfoService.tableInfo[this.workFlowTable]['displayedColumns']
+
+          _nbMenu.onItemSelect().subscribe(x => {
+            x.item.selected = false;
+          })
         }
-        else console.log("Bsak");
+        else this.workFlowTable = 'TCI';
+        this.selectedTable = this.tableNames[0];
       }
     })
   }
- 
-  
+
+  ngOnInit() {
+
+  }
+
 }
